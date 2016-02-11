f583cbeb622733ff80e5d7afb4d983dfa865a3ce
Felix Xu <xuchenfei@gmail.com>
2016-02-12 02:25:23 +0800

    mysql done

--- mysql/script/build.sh
+++ mysql/script/build.sh
@@ -3,9 +3,8 @@ MYSQL_VERSION=5.5
 apt-get update
 apt-get dist-upgrade
 
-apt-get install -y perl --no-install-recommends && rm -rf /var/lib/apt/lists/*
-apt-get install -y libaio1 pwgen && rm -rf /var/lib/apt/lists/*
-apt-get clean && rm -rf /var/lib/apt/lists/*
+apt-get install -y perl --no-install-recommends 
+apt-get install -y libaio1 pwgen 
 
 { \
 		echo mysql-server mysql-server/data-dir select ''; \
@@ -13,7 +12,8 @@ apt-get clean && rm -rf /var/lib/apt/lists/*
 		echo mysql-server mysql-server/re-root-pass password ''; \
 		echo mysql-server mysql-server/remove-test-db select false; \
 	} | debconf-set-selections 
-apt-get update && apt-get install -y mysql-server="${MYSQL_VERSION}"* && rm -rf /var/lib/apt/lists/*
+apt-get install -y mysql-server="${MYSQL_VERSION}"* 
+apt-get clean && rm -rf /var/lib/apt/lists/*
 
 rm -rf /var/lib/mysql && mkdir -p /var/lib/mysql && chown -R mysql:mysql /var/lib/mysql
 sed -Ei 's/^(bind-address|log)/#&/' /etc/mysql/my.cnf
