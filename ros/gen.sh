079fa9e15d07b011c30257cd6edb4242e37465ed
Felix Xu <xuchenfei@gmail.com>
2019-04-27 07:55:39 +0000

    add ros

---
+++ ros/gen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CNIPS=`curl -fsSL https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt | sed -e 's/^/add list=chnips address=/'`
+cat > cnips.rsc << EOF
+/ip firewall address-list remove [/ip firewall address-list find list=chnips]
+/ip firewall address-list
+$CNIPS
+EOF
+scp cnips.rsc bigfei@ros:/
+ssh  ros "/import cnips.rsc"
