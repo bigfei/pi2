150d6ad6d7c6fe1c952484c35afd1aceaf032919
Felix Xu <xuchenfei@gmail.com>
2019-05-02 12:23:45 +0000

    unicom ok

--- ros/ros-cnips.sh
+++ ros/ros-cnips.sh
@@ -6,16 +6,6 @@ cat > cnips.rsc << EOF
 $CNIPS
 EOF
 
-UNI=`curl -fsSL https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/unicom.txt | sed -e 's/^/add list=uni address=/'`
-cat > uni.rsc << EOF
-/ip firewall address-list remove [/ip firewall address-list find list=uni]
-/ip firewall address-list
-$UNI
-EOF
-
 scp cnips.rsc bigfei@ros:/
 ssh bigfei@ros "/import cnips.rsc"
-
-scp uni.rsc bigfei@ros:/
-ssh bigfei@ros "/import uni.rsc"
 
---
+++ ros/ros-uni.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ssh bigfei@ros ":put [/ip address get [find where interface=pppoe-uni] value-name=network]" >  whitelist-uni.txt
+ssh bigfei@ros ":put [/ip address get [find where interface=pppoe-uni] value-name=address]" >>  whitelist-uni.txt
+echo "whitelist is: "
+cat whitelist-uni.txt
+
+UNI=`curl -fsSL https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/unicom.txt | ./cidrmerge whitelist-uni.txt | sed -e 's/^/add list=uni address=/'`
+cat > uni.rsc << EOF
+/ip firewall address-list remove [/ip firewall address-list find list=uni]
+/ip firewall address-list
+$UNI
+EOF
+
+scp uni.rsc bigfei@ros:/
+ssh bigfei@ros "/import uni.rsc"
+
